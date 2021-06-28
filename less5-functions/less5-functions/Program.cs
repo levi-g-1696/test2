1e8d6ca5cf157c60b3ed45be7239d93fe123a9c8
levi-g-1696 <86431818+levi-g-1696@users.noreply.github.com>
2021-06-28 19:12:36 +0300

    Add files via upload

---
+++ less5-functions/less5-functions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace less5_functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print();
+        }
+        public static void Print()
+        {
+
+            Console.WriteLine("printing");Console.WriteLine("printing");Console.WriteLine("printing");
+            Console.WriteLine("printing"); Console.WriteLine(DateTime.Now);
+        }
+    }
+}
